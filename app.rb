5658a073cc084cffc9fb20f629a2c77fe54e419b
yuki_yano <yuki_yano@dwango.co.jp>
2015-01-29 15:50:03 +0900

    first commit

---
+++ app.rb
@@ -0,0 +1,29 @@
+require 'RMagick'
+require 'open-uri'
+require 'sinatra'
+
+set :public_folder, File.dirname(__FILE__) + '/tmp'
+
+helpers do
+  def random_str
+    Array.new(8){[*:a..:z,*0..9].sample}.join
+  end
+end
+
+get '/index' do
+  erb :index
+end
+
+get '/diff' do
+  background_image = Magick::ImageList.new('background.jpg')
+  black_image = Magick::ImageList.new('black.jpg')
+  composite_image = Magick::ImageList.new.from_blob(open(params[:url]).read)
+
+  mask_image = background_image.composite(composite_image, 0, 0, Magick::DifferenceCompositeOp).threshold(250)
+  @filename = "#{random_str}.png"
+
+  save_image = black_image.composite(composite_image.composite(mask_image, 0, 0, Magick::CopyOpacityCompositeOp), 0, 0, Magick::OverCompositeOp)
+  save_image.write("./tmp/#{@filename}")
+
+  erb :diff
+end
