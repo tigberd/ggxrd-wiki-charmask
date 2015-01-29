f557edee626429baff33ab37133b5df253b15ad0
yuki_yano <yuki_yano@dwango.co.jp>
2015-01-29 17:59:37 +0900

    Add mask file

--- app.rb
+++ app.rb
@@ -20,10 +20,14 @@ get '/diff' do
   composite_image = Magick::ImageList.new.from_blob(open(params[:url]).read)
 
   mask_image = background_image.composite(composite_image, 0, 0, Magick::DifferenceCompositeOp).threshold(250)
-  @filename = "#{random_str}.png"
 
   save_image = black_image.composite(composite_image.composite(mask_image, 0, 0, Magick::CopyOpacityCompositeOp), 0, 0, Magick::OverCompositeOp)
+
+  @filename = "#{random_str}.png"
   save_image.write("./tmp/#{@filename}")
+
+  @mask_filename = "#{random_str}_mask.png"
+  mask_image.write("./tmp/#{@mask_filename}")
 
   erb :diff
 end
